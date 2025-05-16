152ce1e31b447620767ce9af1cfb121d8a665a5c
Veer1578 <sweetyatulshah@gmail.com>
2025-05-16 09:26:45 +0530

    intersection

---
+++ intersection.py
@@ -0,0 +1,8 @@
+setX = {'green', 'blue'}
+setY = {'yellow', 'blue'}
+print('Orignal elements of sets:')
+print(setX)
+print(setY)
+print('Intersection of two said sets:')
+setZ = setX.intersection(setY)
+print(setZ)
